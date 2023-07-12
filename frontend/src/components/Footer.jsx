70314edb314e8a04795bbe4922f068f26d3e0726
dattlab <122553391+dattlab@users.noreply.github.com>
2023-07-12 18:33:12 +0800

    Fix footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -20,35 +20,35 @@ const Footer = () => {
     <footer>
       <Container className='footer-cont pb-3 pt-5' fluid>
         <Container className='d-flex footer-main-row justify-content-center'>
-          <Col md={4} sm={12} className='footer-logo mb-3'>
+          <Col md={12} sm={12} className='footer-logo mb-3'>
             <Link to='/'>
               <Image className='footer-logo' src='/logo.png' />
             </Link>
           </Col>
 
-          <Col md={8} className='footer-contacts'>
-            <Row>
-              <h2 className='s-at-k' style={{ fontSize: '2.5rem' }}>
+          <Col sm={12} className='d-flex flex-column footer-contacts'>
+            <Row sm={12}>
+              <h2 className='s-at-k footer-br'>
                 {brandName}
               </h2>
             </Row>
 
-            <Row>
+            <Row sm={12}>
               <Col className='mb-4' lg={4} md={12}>
                 <h5>Contact</h5>
-                <div>
+                <div className='footer-items'>
                   <b>{phoneIcon} </b>
                   <a href='tel:+631234567890' className='social-link'>
                     {navPhoneNumber}
                   </a>
                 </div>
-                <div>
+                <div className='footer-items'>
                   <b>{emailIcon} </b>
                   <a href='mailto:example@email.com' className='social-link'>
                     {navEmail}
                   </a>
                 </div>
-                <div>
+                <div className='footer-items'>
                   <b>{addressIcon} </b>
                   10 St. Peter Street, San Jose Village, Muntinlupa City, Metro
                   Manila
@@ -56,7 +56,7 @@ const Footer = () => {
               </Col>
               <Col className='mb-4' md={4} sm={6} xs={12}>
                 <h5>Follow</h5>
-                <div>
+                <div className='footer-items'>
                   {fbIcon} {}
                   <a
                     href='https://facebook.com'
@@ -66,7 +66,7 @@ const Footer = () => {
                     Facebook {extLinkIcon}
                   </a>
                 </div>
-                <div>
+                <div className='footer-items'>
                   {twtIcon} {}
                   <a
                     href='https://twitter.com'
@@ -76,7 +76,7 @@ const Footer = () => {
                     Twitter {extLinkIcon}
                   </a>
                 </div>
-                <div>
+                <div className='footer-items'>
                   {instaIcon} {}
                   <a
                     href='https://instagram.com'
@@ -89,12 +89,12 @@ const Footer = () => {
               </Col>
               <Col>
                 <h5>About</h5>
-                <div>
+                <div className='footer-items'>
                   <Link to='/about' className='social-link'>
                     Our Business
                   </Link>
                 </div>
-                <div>
+                <div className='footer-items'>
                   <Link to='/blog' className='social-link'>
                     Our Blog
                   </Link>
