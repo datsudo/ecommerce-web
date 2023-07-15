6529c1977bda559d75f371916fb7a7507eb8d66b
dattlab <122553391+dattlab@users.noreply.github.com>
2023-07-15 18:57:47 +0800

    Fix server

--- backend/server.js
+++ backend/server.js
@@ -43,8 +43,9 @@ app.get('/api/config/paypal', (req, res) =>
 // } else {
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.get('/', (req, res) => {
-    res.send('API is running....');
+app.get('/', (req, res) => {
+  // res.send('API is running....');
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
 });
 // }
 
