d2356e52785fdd2ea8e1c745d09c166f5933a6f5
dattlab <122553391+dattlab@users.noreply.github.com>
2023-07-15 16:26:44 +0800

    Update root url

--- backend/server.js
+++ backend/server.js
@@ -39,11 +39,12 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
+
 } else {
   const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-  app.get('/', (req, res) => {
-    res.send('API is running....');
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    app.get('/', (req, res) => {
+      res.send('API is running....');
   });
 }
 
