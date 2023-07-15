514c5487998478e0965ffda5d852ce686674dc3b
dattlab <122553391+dattlab@users.noreply.github.com>
2023-07-15 19:07:14 +0800

    Fix server

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,7 @@ app.get('/api/config/paypal', (req, res) =>
 // } else {
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('/', (req, res) => {
   // res.send('API is running....');
   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
