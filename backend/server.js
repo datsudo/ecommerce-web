4a1d8f37d0c01bd3c6d5907e351dda6fb551b0ac
dattlab <122553391+dattlab@users.noreply.github.com>
2023-07-15 15:39:09 +0800

    Fix cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 import connectDB from './config/db.js';
@@ -16,6 +17,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
